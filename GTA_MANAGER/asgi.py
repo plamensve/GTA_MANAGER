c3fcfcf34917fc65790bfc5cd67178ad2ca42d6f
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2024-12-15 19:21:30 +0200

    ADD: First test commit

---
+++ GTA_MANAGER/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for GTA_MANAGER project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GTA_MANAGER.settings')
+
+application = get_asgi_application()
